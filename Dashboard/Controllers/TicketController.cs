b10186939e3b22034ebcbbff028d2c2ec2629f19
Muzamil Dev <Muzamil Dev@DESKTOP-RRVVA3N>
2023-12-20 21:47:58 +0500

    added change Department loging and its view and call to action for that in  Ticket => Indexs

--- Dashboard/Controllers/TicketController.cs
+++ Dashboard/Controllers/TicketController.cs
@@ -133,20 +133,36 @@ namespace Dashboard.Controllers
             //var test = result.Adapt<IEnumerable<BillingInfoDto>>();
             return RedirectToAction("Index");
         }
-        //[HttpPost]
-        //public IActionResult Edit(TicketDto obj)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var test = obj.Adapt<Ticket>();
 
-        //        _unitOfWork.ticket.update(test);
-        //        _unitOfWork.Save();
-        //    }
+        [HttpGet]
+        public IActionResult ChangeDepartment(int id)
+        {
+            var result = _unitOfWork.ticket.Get(id);
+            //var result = _unitOfWork.ticket.CustomeGetAll().Where(x => x.Id == id).AsNoTracking().FirstOrDefaultAsync();
+            if (result != null)
+            {
+                var deparments = new List<string> { "Admin", "Tickitting", "Billing" };
+                ViewBag.Departments = new SelectList(deparments);
+                var result2 = result.Adapt<TicketDto>();
+                return View(result2);
+            }
+            return View();
+        }
 
-        //    //var test = result.Adapt<IEnumerable<BillingInfoDto>>();
-        //    return RedirectToAction("Index");
-        //}
+        [HttpPost]
+        public IActionResult ChangeDepartment(TicketDto obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var test = obj.Adapt<Ticket>();
+
+                _unitOfWork.ticket.update(test);
+                _unitOfWork.Save();
+            }
+
+            //var test = result.Adapt<IEnumerable<BillingInfoDto>>();
+            return RedirectToAction("Index");
+        }
         public IActionResult Closed()
         {
             //var result =  _unitOfWork.ticket.GetAll();
